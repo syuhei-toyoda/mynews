46ce37ad7e018b4aba13dd1e291bc00d0cc041c5
s-toyoda <syuhei.toyoda@fusionia.net>
2021-03-31 22:27:47 +0900

    課題を再提出します

--- routes/web.php
+++ routes/web.php
@@ -18,10 +18,8 @@ Route::get('/', function () {
 });
 Route::group(['prefix' => 'admin'], function() {
     Route::get('news/create', 'Admin\NewsController@add');
-  });
-  Route::group(['prefix' => 'admin'], function() {
+  
     Route::get('admin/profile/create', 'Admin\ProfileController@add');
-  });
-  Route::group(['prefix' => 'admin'], function() {
+  
     Route::get('admin/profile/edit', 'Admin\ProfileController@edit');
   });
