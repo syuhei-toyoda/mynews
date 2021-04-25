93153481c9f4fc00e986a57550cca3ed590fda00
s-toyoda <syuhei.toyoda@fusionia.net>
2021-04-25 21:31:40 +0900

    課題を再提出します

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -15,8 +15,41 @@ class ProfileController extends Controller
     return view('admin.profile.create');
 }
 
-public function create()
+public function create(Request $request)
 {
+    
+
+        // 以下を追記
+        // Varidationを行う
+        $this->validate($request, Profile::$rules);
+  
+        $profile = new Profile;
+        $form = $request->all();
+  
+        // フォームから送信されてきた_tokenを削除する
+        unset($form['_token']);
+        // フォームから送信されてきたimageを削除する
+        unset($form['image']);
+  
+        // データベースに保存する
+        $profile->fill($form);
+        $profile->save();
+  
+        return redirect('admin/profile/create');
+    }
+       public function index(Request $request)
+    {
+        $cond_title = $request->cond_title;
+        if ($cond_title != '') {
+            // 検索されたら検索結果を取得する
+            $posts = Profile::where('title', $cond_title)->get();
+        } else {
+            // それ以外はすべてのニュースを取得する
+            $posts = Profile::all();
+        }
+        return view('admin.profile.index', ['posts' => $posts, 'cond_title' => $cond_title]);
+    
+  
     return redirect('admin/profile/create');
 }
 
@@ -29,45 +62,7 @@ public function update()
 {
     return redirect('admin/profile/edit');
 }
-  {
+  
 
-      // 以下を追記
-      // Varidationを行う
-      $this->validate($request, Profile::$rules);
-
-      $news = new Profile;
-      $form = $request->all();
-
-      // フォームから画像が送信されてきたら、保存して、$news->image_path に画像のパスを保存する
-      if (isset($form['image'])) {
-        $path = $request->file('image')->store('public/image');
-        $news->image_path = basename($path);
-      } else {
-          $news->image_path = null;
-      }
-
-      // フォームから送信されてきた_tokenを削除する
-      unset($form['_token']);
-      // フォームから送信されてきたimageを削除する
-      unset($form['image']);
-
-      // データベースに保存する
-      $profile->fill($form);
-      $profile->save();
-
-      return redirect('admin/profile/create');
-  }
-     public function index(Request $request)
-  {
-      $cond_title = $request->cond_title;
-      if ($cond_title != '') {
-          // 検索されたら検索結果を取得する
-          $posts = Profile::where('title', $cond_title)->get();
-      } else {
-          // それ以外はすべてのニュースを取得する
-          $posts = Profile::all();
-      }
-      return view('admin.profile.index', ['posts' => $posts, 'cond_title' => $cond_title]);
-  }
 
 }
